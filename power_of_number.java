909454ad01c119624e6861a2f21a2a684f6b1f3d
siddiqsidd <96865056+siddiqsidd@users.noreply.github.com>
2022-01-30 13:13:46 +0530

    Add files via upload

---
+++ power_of_number.java
@@ -0,0 +1,12 @@
+package jan25;
+import java.util.*;
+public class power_of_number {
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		double a=s.nextDouble();
+		double b=s.nextDouble();
+		double c=Math.pow(a, b);
+		System.out.println(c);
+	}
+
+}
